1fdeb2fc72c8abc4e579537c8ac9d07e542df009
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-11-12 22:56:39 +0530

    Added login function

--- Module 2/Day_5_Github/Q12/signup.js
+++ Module 2/Day_5_Github/Q12/signup.js
@@ -12,3 +12,21 @@ function signup(userName) {
 // Example:
 console.log(signup("ram"));     
 console.log(signup("newUser")); 
+
+
+function login(userName, password) {
+  let users = ["ram", "sita", "lakshmi", "krishna"];
+
+  if (!users.includes(userName)) {
+    return "User Not Found, Please Signup";
+  } else if (password !== "Emp@123") {
+    return "Wrong Password....";
+  } else {
+    return "Login Successful...";
+  }
+}
+
+
+console.log(login("ram", "Emp@123")); 
+console.log(login("sita", "wrong"));  
+console.log(login("newUser", "Emp@123"));
