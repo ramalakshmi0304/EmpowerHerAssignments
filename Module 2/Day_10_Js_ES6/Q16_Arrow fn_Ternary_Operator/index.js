48b84adc419d8ff3cf690460f3cadfb6d9ed7073
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-11-19 15:29:28 +0530

    Arrow fn & Ternary operator

---
+++ Module 2/Day_10_Js_ES6/Q16_Arrow fn_Ternary_Operator/index.js
@@ -0,0 +1,17 @@
+/*arrow function isEven(n)*/
+const isEven=(n)=>n%2==0
+
+console.log(isEven(10)); 
+console.log(isEven(7)); 
+
+// Ternary Operator Version
+
+const result=marks >=35 ? "Pass": "Fail"
+
+// c) Write an arrow function greet(name) that prints: "Hello, " If no name is passed, print "Guest" using the ternary operator.
+const greet=(name)=>
+
+    console.log(`Hello, ${name ? name:"Guest"}`)
+
+    greet("Rama");
+    greet();
