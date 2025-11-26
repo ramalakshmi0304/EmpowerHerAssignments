40f90c03e1ccc1b94914688d02095f7d16c76d65
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-11-26 11:48:12 +0530

    async callback fn executed

---
+++ Module 2/Day_14_Promises_callback/Q12_Async_callback/index.js
@@ -0,0 +1,14 @@
+function timer(duration,onComplete){
+ 
+  
+  setTimeout(() => {
+    onComplete (`Timer of ${duration} ms finished`)
+    
+  }, duration);
+}
+
+
+timer(2000,(message)=>{
+  console.log(message);
+});
+
