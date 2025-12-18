dd5a1333b87116eebeb9df3e40007af8a75d3199
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2025-12-18 16:00:52 +0530

    context api assignment done

---
+++ Module 3/React/Day_11/q-context-api/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component1 from "./Component1";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Component1/>
+    </div>
+  );
+}
+
+export default App
---
+++ Module 3/React/Day_11/q-context-api/src/Component1.jsx
@@ -0,0 +1,22 @@
+import { AppContext } from "./AppContext";
+import Component2 from "./Component2";
+
+const Component1 = () => {
+
+  const values = {
+    a: "Apple",
+    b: "Banana",
+    c: "Cat",
+    d: "Dog",
+    e: "Elephant",
+    f: "Fish",
+  };
+
+  return (
+    <AppContext.Provider value={values}>
+      <Component2 />
+    </AppContext.Provider>
+  );
+};
+
+export default Component1;
